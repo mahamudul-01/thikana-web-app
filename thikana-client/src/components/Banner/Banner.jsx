e796525c5cbc94c1491173a092b6cc10b70ceaa7
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-03-11 14:58:33 +0600

    add filer buttons

--- thikana-client/src/components/Banner/Banner.jsx
+++ thikana-client/src/components/Banner/Banner.jsx
@@ -1,22 +1,104 @@
-import banner_image from '../../assets/images/banner/banner.png'
+import banner_image from "../../assets/images/banner/banner.png";
 
 const Banner = () => {
-    return (
-        <div className='mt-6'>
-            <div className="bg py-20" style={{ backgroundImage: `url(${banner_image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                <div>
-                    <h2 className='text-4xl font-bold text-white mx-20 text-center'>Seeking Your Space?</h2>
-                    <p className='text-sm text-gray-400 text-center mt-3'>Seize the opportunity to own your dream property <br />
-                    invest in your future today!</p>
-                    <div className='text-center mt-6'>
-                        <button className='border border-[#0C7C1D] rounded-l-xl  px-3 py-1 text-base bg-[#56705a36]  text-white '>Rent</button>
-                        <button className='border border-[#0C7C1D] rounded-r-xl  px-3 py-1 text-base bg-[#0C7C1D]  text-white '>Buy</button>
-                        
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="mt-6">
+      <div
+        className="bg py-20"
+        style={{
+          backgroundImage: `url(${banner_image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div>
+          <h2 className="text-4xl font-bold text-white mx-20 text-center">
+            Seeking Your Space?
+          </h2>
+          <p className="text-sm text-gray-400 text-center mt-3">
+            Seize the opportunity to own your dream property <br />
+            invest in your future today!
+          </p>
+          <div className="text-center mt-6">
+            <button className="border border-[#0C7C1D] rounded-l-xl  px-3 py-1 text-base bg-[#56705a36]  text-white ">
+              Rent
+            </button>
+            <button className="border border-[#0C7C1D] rounded-r-xl  px-3 py-1 text-base bg-[#0C7C1D]  text-white ">
+              Buy
+            </button>
+          </div>
+          <div className="flex justify-center w-full gap-6  ">
+           <div className=" mt-6 ">
+           <select className="select   rounded-2xl w-52 select-success">
+              <option disabled selected>
+             Location
+              </option>
+              <option>Java</option>
+              <option>Go</option>
+              <option>C</option>
+              <option>C#</option>
+              <option>C++</option>
+              <option>Rust</option>
+              <option>JavaScript</option>
+              <option>Python</option>
+            </select>
+           </div>
+           <div className=" mt-6 ">
+           <select className="select   rounded-2xl w-52 select-success">
+              <option disabled selected>
+             Category
+              </option>
+              <option>Java</option>
+              <option>Go</option>
+              <option>C</option>
+              <option>C#</option>
+              <option>C++</option>
+              <option>Rust</option>
+              <option>JavaScript</option>
+              <option>Python</option>
+            </select>
+           </div>
+           <div className=" mt-6 ">
+           <select className="select   rounded-2xl w-52 select-success">
+              <option disabled selected>
+             Type
+              </option>
+              <option>Java</option>
+              <option>Go</option>
+              <option>C</option>
+              <option>C#</option>
+              <option>C++</option>
+              <option>Rust</option>
+              <option>JavaScript</option>
+              <option>Python</option>
+            </select>
+           </div>
+           <div className=" mt-6 ">
+           <select className="select   rounded-2xl w-52 select-success">
+              <option disabled selected>
+             Status
+              </option>
+              <option>Java</option>
+              <option>Go</option>
+              <option>C</option>
+              <option>C#</option>
+              <option>C++</option>
+              <option>Rust</option>
+              <option>JavaScript</option>
+              <option>Python</option>
+            </select>
+           </div>
+         
+          </div>
+          <div className="flex justify-center">
+          <button className="w-1/4 mt-6  rounded-2xl  py-2 text-base bg-[#0C7C1D]  text-white ">
+              Find
+          </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
